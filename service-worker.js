eb1614b6373d7277658042e2e5dbd26c431d7b1c
Jeremy King <kingpinzs@gmail.com>
2024-08-09 15:25:54 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,6 +2,8 @@ const CACHE_NAME = 'delivery-calculator-cache-v1';
 const urlsToCache = [
     '/',
     '/index.html',
+    '/manifest.json', // Make sure the manifest is cached too
+    '/icon.png',   // Add any icons or images you might be using
     '/service-worker.js'
 ];
 
@@ -11,6 +13,21 @@ self.addEventListener('install', (event) => {
             .then((cache) => {
                 return cache.addAll(urlsToCache);
             })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 });
 
